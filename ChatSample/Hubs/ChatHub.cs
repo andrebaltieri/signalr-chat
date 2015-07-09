a5bb9f5ee05509c55aaa3642f3370b0580663da1
Andre Baltieri <andrebaltieri@hotmail.com>
2015-07-09 18:39:46 -0300

    Iniciado repo

---
+++ ChatSample/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace ChatSample.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.addMessage(message);
+        }
+    }
+}
---
+++ ChatSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin.Cors;
+using Owin;
+
+namespace ChatSample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+}
